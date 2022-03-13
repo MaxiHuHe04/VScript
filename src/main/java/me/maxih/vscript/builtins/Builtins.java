31363d1118778a2e1d5625dc6c3c5359f387c001
MaxiHuHe04 <maxiherczegh@outlook.com>
2022-03-13 01:14:18 +0100

    Renamed print and println parameters to prevent misunderstandings

--- src/main/java/me/maxih/vscript/builtins/Builtins.java
+++ src/main/java/me/maxih/vscript/builtins/Builtins.java
@@ -6,17 +6,17 @@ public class Builtins {
 
     public static BuiltinFunction[] builtinFunctions = {
             new BuiltinFunction("print", space -> {
-                Object object = space.get("object");
+                Object object = space.get("value");
                 if (object instanceof Object[] array) System.out.print(Arrays.toString(array));
                 else System.out.print(object);
                 return null;
-            }, "object"),
+            }, "value"),
             new BuiltinFunction("println", space -> {
-                Object object = space.get("object");
+                Object object = space.get("value");
                 if (object instanceof Object[] array) System.out.println(Arrays.toString(array));
                 else System.out.println(object);
                 return null;
-            }, "object")
+            }, "value")
     };
 
 }
